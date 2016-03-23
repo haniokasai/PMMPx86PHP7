24c7934cd761068479c7794719581c195756c3f7
haniokasai <htek@haniokasai.com>
2016-03-23 19:43:25 +0900

    Update update-genisys.bat

--- update-genisys.bat
+++ update-genisys.bat
@@ -1,5 +1,5 @@
 del PocketMine-MP.phar
-utils\bin\wget.exe http://jenkins.mcper.cn/job/Genisys-master/lastSuccessfulBuild/artifact/releases/*zip*/releases.zip
+utils\bin\wget.exe http://jenkins.mcper.cn/job/Genisys-master/lastSuccessfulBuild/artifact/*zip*/archive.zip
 utils\bin\unzip.exe releases.zip
 utils\bin\cp.exe releases\*.phar PocketMine-MP.phar
 del /Q releases 
