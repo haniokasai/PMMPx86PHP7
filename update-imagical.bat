44b53d6f4ab020a790c0820d68d30c45f2168a58
haniokasai♡ <htek@haniokasai.com>
2016-04-02 17:15:41 +0900

    Update update-imagical.bat

--- update-imagical.bat
+++ update-imagical.bat
@@ -1,6 +1,2 @@
 del PocketMine-MP.phar
-utils\bin\wget.exe http://jenkins.imagicalmine.net/job/ImagicalMine/lastSuccessfulBuild/artifact/releases/*zip*/releases.zip
-utils\bin\unzip.exe releases.zip
-utils\bin\cp.exe releases\*.phar PocketMine-MP.phar
-del /Q releases 
-del /Q releases.zip
+utils\bin\wget.exe http://imagicalmine.github.io/ImagicalMine/service-download/ImagicalMine.phar
